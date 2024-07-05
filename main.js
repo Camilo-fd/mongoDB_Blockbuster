cf05696a2e4a2233f8c5e746af295e6587eea138
Camilo-fd <156461877+Camilo-fd@users.noreply.github.com>
2024-07-04 21:28:23 -0500

    feat: :sparkles: Nueva connecion a la base de datos

--- main.js
+++
@@ -1,7 +0,0 @@
-import { getActorsAwardsOscar, getAllAwardsActor, getAllActors1980 } from "./js/model/authors.js";
-import { getMovilById } from "./js/model/movis.js";
-
-// console.log(await getMovilById());
-// console.log(await getActorsAwardsOscar());
-// console.log(await getAllAwardsActor());
-console.log(await getAllActors1980());
